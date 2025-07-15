2f0dd25003461ff60be7615daf6a74ab597eba89
arjsabbir88 <arjsabbir88@gmail.com>
2025-07-16 01:12:53 +0600

    completed added policies by the admin

--- index.js
+++ index.js
@@ -1,6 +1,62 @@
-const express = require('express');
-const app = express();
-const port = process.env.port || 3000;
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
+const port = process.env.PORT || 3000;
+
+
+app.use(express.json());
+app.use(cors());
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.DB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // create a database and collection list
+    const database = client.db('LifeSure');
+
+    // policiesCollection
+    const policiesCollection = database.collection('policies');
+
+
+    //insert new policy
+    app.post('/policies', async(req,res)=>{
+        const policy = req.body;
+        const result = await policiesCollection.insertOne(policy);
+        res.send(result);
+    })
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 
 app.get('/', (req, res)=>{
