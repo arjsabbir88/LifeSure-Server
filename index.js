a6df632d2359583436025fef92cee1649bf00298
arjsabbir88 <arjsabbir88@gmail.com>
2025-07-20 02:12:49 +0600

    created api for the top policies by mongodb aggregation query method

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 3000;
-
 
 app.use(express.json());
 app.use(cors());
@@ -15,7 +14,7 @@ const client = new MongoClient(process.env.DB_URI, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -24,46 +23,94 @@ async function run() {
     await client.connect();
 
     // create a database and collection list
-    const database = client.db('LifeSure');
+    const database = client.db("LifeSure");
 
     // policiesCollection
-    const policiesCollection = database.collection('policies');
+    const policiesCollection = database.collection("policies");
     // created bookingPolicyCollection
-    const bookingPolicyCollection = database.collection('bookingPolicyCollection');
-
+    const bookingPolicyCollection = database.collection(
+      "bookingPolicyCollection"
+    );
 
     //insert new policy
-    app.post('/policies', async(req,res)=>{
-        const policy = req.body;
-        const result = await policiesCollection.insertOne(policy);
-        res.send(result);
-    })
+    app.post("/policies", async (req, res) => {
+      const policy = req.body;
+      const result = await policiesCollection.insertOne(policy);
+      res.send(result);
+    });
 
     // get all policies
-    app.get('/policies', async(req, res)=>{
-        const policies = await policiesCollection.find().toArray();
-        res.send(policies);
-    })
+    app.get("/policies", async (req, res) => {
+      const policies = await policiesCollection.find().toArray();
+      res.send(policies);
+    });
 
     // get a single policy by id
-    app.get('/policies/:id', async(req,res)=>{
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id)}
-        const policy = await policiesCollection.findOne(query);
-        res.send(policy);
-    })
+    app.get("/policies/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const policy = await policiesCollection.findOne(query);
+      res.send(policy);
+    });
 
     // created api for the booking policy collection
-    app.post('/booking-policy',async(req,res)=>{
+    app.post("/booking-policy", async (req, res) => {
       const bookingPolicy = req.body;
-      const bookedPolicy = await bookingPolicyCollection.insertOne(bookingPolicy);
+      const bookedPolicy = await bookingPolicyCollection.insertOne(
+        bookingPolicy
+      );
       res.send(bookedPolicy);
-    })
+    });
 
+    // count booking data on the booking collection with mongodb aggregation query method
+
+    app.get("/top-policies", async (req, res) => {
+      try {
+        const topPolicies = await bookingPolicyCollection
+          .aggregate([
+            {
+              $group: {
+                _id: { $toObjectId: "$bookingPolicyId" },
+                totalBookings: { $sum: 1 },
+              },
+            },
+            { $sort: { totalBookings: -1 } },
+            { $limit: 6 },
+            {
+              $lookup: {
+                from: "policies",
+                localField: "_id",
+                foreignField: "_id",
+                as: "policyInfo",
+              },
+            },
+            { $unwind: "$policyInfo" },
+            {
+              $project: {
+                policyId: "$_id",
+                totalBookings: 1,
+                name: "$policyInfo.policyTitle",
+                category: "$policyInfo.category",
+                image: "$policyInfo.imageUrl",
+                premium: "$policyInfo.basePremium",
+                coverageRange: "$policyInfo.coverageRange",
+              },
+            },
+          ])
+          .toArray();
+
+        res.send(topPolicies);
+      } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: "Something went wrong" });
+      }
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -71,12 +118,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Life Sure insurance server is running");
+});
 
-
-app.get('/', (req, res)=>{
-    res.send('Life Sure insurance server is running');
-})
-
-app.listen(port, ()=>{
-    console.log(`Server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
